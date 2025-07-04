aae658973ada7975ac102b0bffcea0ed79810486
Daniel Sadiq <danielsadiq93@gmail.com>
2025-07-04 10:51:09 +0100

    without config

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+/**
+ * Node modules
+ */
+
+import express from "express";
+/**
+ * Express app initial
+ */
+
+const app = express();
+
+app.listen(3000, ()=> {
+  console.log('Server running: http://localhost:3000')
+})
